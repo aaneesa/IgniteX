2317bbf156e3d293700b15c3184a67ece8d0b431
aaneesa <anweshaa002@gmail.com>
2025-11-04 23:13:47 +0530

    problems and problems

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import { generateToken } from "./jwt.js";
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:3000", "https://ignite-x-five.vercel.app/login"],
+    origin: ["http://localhost:3000", "https://ignite-x-five.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
